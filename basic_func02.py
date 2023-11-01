009abb77782d319c7e126854ea7e3aabc055dffd
Zafarbek157 <zafarbek.saidaxmadov2811@gmail.com>
2023-11-01 13:57:22 +0500

    added solutions

--- basic_func02.py
+++ basic_func02.py
@@ -1,3 +1,5 @@
 # Create a function called main.
-
+def main():
 # Return the value "Hello World"
+    return("hello world")
+print(main())
--- basic_func10.py
+++ basic_func10.py
@@ -1,5 +1,6 @@
 # Create a function called main.
-
+def main(a):
 # Create function arguments a.
-
+  return 0-a
 # Return the negative value of a.
+print(main(0-a))
